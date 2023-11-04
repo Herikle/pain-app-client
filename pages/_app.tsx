34feb27c0d5f07f887522effb55d4701ce663adb
gabrielMes <gabriel.mesquita352@gmail.com>
2023-11-04 00:16:39 -0300

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,6 +29,26 @@ const Application = ({ children }: ApplicationProps) => {
       <main>
         <Head>
           <title>PainTrack</title>
+          <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg"/>
           <meta
             name="description"
             content="A scientific tool for the description and analysis of the pain experience"
